0f597209050a96b9855b662da1fb9233a1800e4b
“Viktor <Viktor.ivaschenkov@raiffeisen.ru>
2022-06-22 14:45:34 +0300

    пробуем меняем

--- reactproject1/src/App.js
+++ reactproject1/src/App.js
@@ -1,16 +1,45 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-        Simple Dimple Popit
-        <ul>
-            <li>Один</li>
-            <li>Два</li>
-        </ul>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div>
+                Simple Dimple Popit
+                <ul>
+                    <li>Один</li>
+                    <li>Два</li>
+                    <li>Три</li>
+                </ul>
+            </div>
+
+            <Technologies />
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href= '#s'>Home</a>
+            <a href= '#s'>News Feed</a>
+            <a href= '#'>Messages</a>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
