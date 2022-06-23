80576237fe33f9300430854aaecfb99d33edf85f
MrCrabs67f <mrcrabs67forms@outlook.com>
2022-06-23 13:35:35 +0300

    123

--- reactproject1/src/App.js
+++ reactproject1/src/App.js
@@ -19,16 +19,19 @@ const App = () => {
 
             <div className='content'>
                 <img src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'></img>
+
+
+                <div className='avatar'>
+                    ava + description
+                </div>
+
+                <div className='posts'>
+                    posts
+                    <div> new posts</div>
+                </div>
+
             </div>
 
-            <div className='avatar'>
-                ava + description
-            </div>
-
-            <div className='posts'>
-                posts
-                <div> new posts</div>
-            </div>
         </div>
     );
 }
